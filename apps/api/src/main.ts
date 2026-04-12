b8e3747bb36f53e3adf80d2053e76511add129ba
Haiylee <haiylee.seo.dev@gmail.com>
2026-04-12 01:50:04 +0100

    check point

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
 
   // CORS 설정 (프론트엔드 버셀 주소 허용)
   app.enableCors({
-    origin: 'https://your-frontend.vercel.app', // 프론트 주소 넣기
+    origin: 'https://fluent-flow-lime.vercel.app/', // 프론트 주소 넣기
     credentials: true,
   });
 
-  // 버셀 환경에서는 process.env.PORT를 사용해야 합니다.
+  // 버셀 환경에서는 process.env.PORT를 사용해야 함
   // await app.listen(process.env.PORT ?? 3000);
   await app.listen(process.env.PORT || 3000);
 }
